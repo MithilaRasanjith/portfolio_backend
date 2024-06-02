7b8d185b2e870afb492ff2864e95c3ff8f6a9ce3
Mithila Wickramathunge <mithilarasanjith@gmail.com>
2024-06-02 13:37:51 +0530

    install cors

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require("express"); // create an express app
 const app = express();
 const port = 5000;
 
+const cors = require("cors");
+app.use(cors());
+
 app.use(express.json());
 
 require("dotenv").config();
